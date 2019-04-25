1cd5978c0cea44696030d88fedc3fe9b44701002
kennylam91 <phamvanlam1991@gmail.com>
2019-04-25 16:11:38 +0700

    add file

--- Bai_tap/bai_5.js
+++ Bai_tap/bai_5.js
@@ -2,65 +2,64 @@ function showMessage() {
 
     let amountInput = document.getElementById('amount').value;
     let amount = parseFloat(amountInput);
-    // let result, fromUsdRate, toUsdRate;
-    // let fromC = document.getElementById('fromCurrency').value;
-    // let toC = document.getElementById('toCurrency').value;
-    // switch (fromC) {
-    //     case("VND"):
-    //         fromUsdRate = 1 / 23000;
-    //         break;
-    //     case("USD"):
-    //         fromUsdRate = 1;
-    //         break;
-    //     case("CNY"):
-    //         fromUsdRate = 1 / 6.5;
-    //         break;
-    //     case("JPY"):
-    //         fromUsdRate = 1 / 112;
-    //         break;
-    //     case("EUR"):
-    //         fromUsdRate = 1.12;
-    //         break;
-    //     case("WON"):
-    //         fromUsdRate = 1 / 1141;
-    //         break;
-    //     case("SGD"):
-    //         fromUsdRate = 1 / 1.36;
-    //         break;
-    //     case("GBP"):
-    //         fromUsdRate = 1.29;
-    //         break;
-    //
-    // }
-    // switch (toC) {
-    //     case("VND"):
-    //         toUsdRate = 23000;
-    //         break;
-    //     case("USD"):
-    //         toUsdRate = 1;
-    //         break;
-    //     case("CNY"):
-    //         toUsdRate = 6.5;
-    //         break;
-    //     case("JPY"):
-    //         toUsdRate = 112;
-    //         break;
-    //     case("EUR"):
-    //         toUsdRate = 1 / 1.12;
-    //         break;
-    //     case("WON"):
-    //         toUsdRate = 1141;
-    //         break;
-    //     case("SGD"):
-    //         toUsdRate = 1.36;
-    //         break;
-    //     case("GBP"):
-    //         toUsdRate = 1 / 1.29;
-    //         break;
-    // }
     let a=eval((document.getElementById("fromCurrency").value));
     let b=eval((document.getElementById("toCurrency").value));
     result = a*b* amount;
     document.getElementById('result').innerHTML = 'Result= ' + result;
-
 }
+// let result, fromUsdRate, toUsdRate;
+// let fromC = document.getElementById('fromCurrency').value;
+// let toC = document.getElementById('toCurrency').value;
+// switch (fromC) {
+//     case("VND"):
+//         fromUsdRate = 1 / 23000;
+//         break;
+//     case("USD"):
+//         fromUsdRate = 1;
+//         break;
+//     case("CNY"):
+//         fromUsdRate = 1 / 6.5;
+//         break;
+//     case("JPY"):
+//         fromUsdRate = 1 / 112;
+//         break;
+//     case("EUR"):
+//         fromUsdRate = 1.12;
+//         break;
+//     case("WON"):
+//         fromUsdRate = 1 / 1141;
+//         break;
+//     case("SGD"):
+//         fromUsdRate = 1 / 1.36;
+//         break;
+//     case("GBP"):
+//         fromUsdRate = 1.29;
+//         break;
+//
+// }
+// switch (toC) {
+//     case("VND"):
+//         toUsdRate = 23000;
+//         break;
+//     case("USD"):
+//         toUsdRate = 1;
+//         break;
+//     case("CNY"):
+//         toUsdRate = 6.5;
+//         break;
+//     case("JPY"):
+//         toUsdRate = 112;
+//         break;
+//     case("EUR"):
+//         toUsdRate = 1 / 1.12;
+//         break;
+//     case("WON"):
+//         toUsdRate = 1141;
+//         break;
+//     case("SGD"):
+//         toUsdRate = 1.36;
+//         break;
+//     case("GBP"):
+//         toUsdRate = 1 / 1.29;
+//         break;
+// }
